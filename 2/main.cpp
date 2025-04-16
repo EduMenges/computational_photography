e4e66867fb347d6c3b6a2dec8f437d02783469ca
Eduardo Menges Mattje <eduardomattje@gmail.com>
2025-04-16 08:54:52 -0300

    Changed to PNG but did not get great results

--- 2/main.cpp
+++ 2/main.cpp
@@ -31,6 +31,12 @@ int main(int argc, char *argv[]) {
     fs::path dir(argv[1]);
 
     ifstream csv(dir / "exposure_times.csv");
+
+    if (!csv.is_open()) {
+        cerr << "Could not open `exposure_times.csv` file\n";
+        return EXIT_FAILURE;
+    }
+
     string line;
     getline(csv, line);
 
@@ -47,7 +53,7 @@ int main(int argc, char *argv[]) {
             exposure_times.push_back(exposure_);
 
             string path((dir / name).string());
-            cv::Mat image = cv::imread(path, cv::IMREAD_COLOR);
+            cv::Mat image = cv::imread(path, cv::IMREAD_ANYCOLOR);
 
             if (image.empty()) {
                 cerr << "Failed to load image " << path << '\n';
@@ -62,6 +68,12 @@ int main(int argc, char *argv[]) {
     curve.reserve(256);
 
     ifstream curve_f(dir / "curve.m");
+
+    if (!curve_f.is_open()) {
+        cerr << "Could not open `curve.m` file\n";
+        return EXIT_FAILURE;
+    }
+
     getline(curve_f, line);
 
     while (getline(curve_f, line)) {
@@ -73,12 +85,12 @@ int main(int argc, char *argv[]) {
         string r_s, g_s, b_s;
 
         if (getline(ss, r_s, ' ') && getline(ss, g_s, ' ') && getline(ss, b_s)) {
-            curve.push_back({stod(b_s), stod(g_s), stod(r_s)});
+            curve.push_back({stod(r_s), stod(g_s), stod(b_s)});
         }
     }
 
     cv::Size size(images[0].size());
-    cv::Mat hdr_image(size, CV_32FC3);
+    cv::Mat hdr_image(size, CV_64FC3);
 
     // Mounting HDR image from loaded images
     for (auto i = 0; i < size.height; ++i) {
@@ -92,21 +104,23 @@ int main(int argc, char *argv[]) {
 
                 for (size_t p = 0; p < 3; ++p) {
                     auto z = pixel[p];
-                    auto curve_value = curve[z][p];
-                    auto actual_exposure = std::exp(curve_value);
-                    auto irradiance = actual_exposure / exposure_times[im];
 
                     if (z > 0 && z < UINT8_MAX) {
+                        auto curve_value = curve[z][p];
+                        auto actual_exposure = std::exp(curve_value);
+                        auto irradiance = actual_exposure / exposure_times[im];
+
                         irradiances[p] += irradiance;
                         irradiance_count[p] += 1;
                     }
                 }
             }
 
-            auto &pixel = hdr_image.at<cv::Vec3f>(i, j);
+            auto &pixel = hdr_image.at<cv::Vec3d>(i, j);
             for (size_t p = 0; p < 3; ++p) {
-                float irradiance =
-                    irradiance_count[p] > 0 ? irradiances[p] / irradiance_count[p] : 0.0f;
+                double irradiance = irradiance_count[p] > 0
+                                        ? irradiances[p] / static_cast<double>(irradiance_count[p])
+                                        : 0.0f;
                 pixel[p] = irradiance;
             }
         }
@@ -115,16 +129,16 @@ int main(int argc, char *argv[]) {
     cv::imwrite("hdr_image.hdr", hdr_image);
 
     // Luminance image
-    cv::Mat luminance_image(size, CV_32FC1);
+    cv::Mat luminance_image(size, CV_64FC1);
     double total_luminance = 0.0;
     size_t count = 0;
 
     for (int i = 0; i < size.height; ++i) {
         for (int j = 0; j < size.width; ++j) {
-            auto hdr_pixel = hdr_image.at<cv::Vec3f>(i, j);
-            auto luminance = hdr_pixel[2] * 0.299f + hdr_pixel[1] * 0.587f + hdr_pixel[0] * 0.114f;
+            auto hdr_pixel = hdr_image.at<cv::Vec3d>(i, j);
+            auto luminance = hdr_pixel[2] * 0.299 + hdr_pixel[1] * 0.587 + hdr_pixel[0] * 0.114;
 
-            luminance_image.at<float>(i, j) = luminance;
+            luminance_image.at<double>(i, j) = luminance;
 
             if (luminance > 0.0) {
                 total_luminance += std::log(luminance + std::numeric_limits<double>::epsilon());
@@ -144,26 +158,26 @@ int main(int argc, char *argv[]) {
 
     for (int i = 0; i < size.height; ++i) {
         for (int j = 0; j < size.width; ++j) {
-            float luminance = luminance_image.at<float>(i, j);
-            float scaled_luminance = ALPHA * luminance / average_luminance;
-            float global_operator = scaled_luminance / (1 + scaled_luminance);
+            double luminance = luminance_image.at<double>(i, j);
+            double scaled_luminance = ALPHA * luminance / average_luminance;
+            double global_operator = scaled_luminance / (1 + scaled_luminance);
 
-            float scale = luminance > 0.0f ? (global_operator / luminance) : 0.0f;
-            cv::Vec3f pixel = hdr_image.at<cv::Vec3f>(i, j) * scale;
-            global_tone_mapped.at<cv::Vec3f>(i, j) = pixel;
+            double scale = luminance > 0.0 ? (global_operator / luminance) : 0.0;
+            cv::Vec3d pixel = hdr_image.at<cv::Vec3d>(i, j) * scale;
+            global_tone_mapped.at<cv::Vec3d>(i, j) = pixel;
         }
     }
 
-    // Convert to 8 bit RGB
+    // Convert to 16 bit RGB
     cv::Mat ldr_image;
-    global_tone_mapped.convertTo(ldr_image, CV_16UC3, 255.0);
-    cv::imwrite("ldr_image.jpg", ldr_image);
+    global_tone_mapped.convertTo(ldr_image, CV_16UC3, 65535.0);
+    cv::imwrite("ldr_image.png", ldr_image);
 
     // Gamma correction
     cv::Mat gamma_corrected;
     cv::pow(global_tone_mapped, 1.0 / 1.6, global_tone_mapped);
-    global_tone_mapped.convertTo(gamma_corrected, CV_16UC3, 255.0);
-    cv::imwrite("ldr_image_gamma_corrected.jpg", gamma_corrected);
+    global_tone_mapped.convertTo(gamma_corrected, CV_16UC3, 65535.0);
+    cv::imwrite("ldr_image_gamma_corrected.png", gamma_corrected);
 
     return EXIT_SUCCESS;
 }
